43bc0d6b500eafbcf13d8376f1b7ba4cc469feb5
Antonny <antonnyruanslley@hotmail.com>
2023-03-21 14:46:23 -0300

    Mudança de design de botões

--- lib/widgets/selectPage/custom_button.dart
+++ lib/widgets/selectPage/custom_button.dart
@@ -16,21 +16,26 @@ class CustomButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: height,
-      width: width,
-      decoration: BoxDecoration(
+    return Material(
+      elevation: 10,
+      shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10),
-        border: Border.all(
-          color: Theme.of(context).colorScheme.secondary,
-          width: 2,
-        ),
       ),
-      child: TextButton(
-        onPressed: onPressed,
-        child: Text(
-          label,
-          style: AppTheme.subtitle1(context),
+      child: Container(
+        height: height,
+        width: width,
+        decoration: BoxDecoration(
+          color: AppTheme.secondyColor,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: TextButton(
+          onPressed: onPressed,
+          child: Text(
+            label,
+            style: AppTheme.subtitle1(context).copyWith(
+              color: AppTheme.primaryColor,
+            ),
+          ),
         ),
       ),
     );
